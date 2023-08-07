ee65533dbee8634afa26fe429c6b2729403cfaf1
unknown <fhxmaster@gmail.com>
2023-08-07 18:23:33 +0700

    initial commit

---
+++ students.js
@@ -0,0 +1,10 @@
+const students = [
+    "adam - m", "adel - f", "ahsanu - m", "edo - m", "andra - m",
+    "aul - f", "azkal - m", "boleng - m", "dapa - m", "depita - f",
+    "dimbe - m", "elsa - f", "fatimah - f", "haura - f", "ugo - m",
+    "parik - m", "jeje - f", "kayla - f", "kenyo - f", "putri - f",
+    "rapi - m", "jaki - m", "mutia - f", "naila - f", "adi - m",
+    "raja - m", "iky - f", "rizqa - f", "selpi - f", "vabel - m",
+    "uwid - f", "wildan - m", "yasin - m", "yessa - f", "panjul - f",
+    "zulfah - f"
+];
