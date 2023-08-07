7de9994a726989a4dfb144279fabcbbd576c0aad
unknown <fhxmaster@gmail.com>
2023-08-07 23:15:47 +0700

    final

--- students.js
+++ students.js
@@ -1,3 +1,7 @@
+const sleepy = [
+    "https://cdn.discordapp.com/attachments/900443909677797396/1138126086350307338/IMG_0968.jpg", "https://cdn.discordapp.com/attachments/900443909677797396/1138126091349926039/IMG_0809.jpg", "https://cdn.discordapp.com/attachments/900443909677797396/1138134801401008149/IMG_0873.jpg", "https://cdn.discordapp.com/attachments/900443909677797396/1138134801686212750/IMG_0149.jpg", "https://cdn.discordapp.com/attachments/900443909677797396/1138134802080469012/IMG_7433.jpg", "https://cdn.discordapp.com/attachments/900443909677797396/1138134802457972816/IMG_7434.jpg"
+];
+
 const students = [
     "adam - m", "adel - f", "ahsanu - m", "edo - m", "andra - m",
     "aul - f", "azkal - m", "boleng - m", "dapa - m", "depita - f",
